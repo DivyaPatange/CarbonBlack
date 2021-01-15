b52ead444c4e227fe46e8817a35c998179e41489
divya <divyapatange0@gmail.com>
2021-01-15 13:31:23 +0530

    change password

--- app/Http/Controllers/ChangePasswordController.php
+++ app/Http/Controllers/ChangePasswordController.php
@@ -11,6 +11,8 @@ use App\User;
 use Auth;
 use Route;
 use App\Rules\MatchOldPassword;
+use App\Mail\ChangePasswordMail;
+use Illuminate\Support\Facades\Mail;
 
 
 class ChangePasswordController extends Controller
@@ -28,8 +30,21 @@ class ChangePasswordController extends Controller
             'renew_password' => ['same:new_password'],
         ]);
    
-        User::find(auth()->user()->id)->update(['password'=> Hash::make($request->new_password), 'password_1' => $request->new_password]);
+        $result = User::find(auth()->user()->id)->update(['password'=> Hash::make($request->new_password), 'password_1' => $request->new_password]);
+        
+        $user = User::findorfail(auth()->user()->id);
+        if($result)
+        {
+            // dd( Mail::to($email)->send(new ResetMail($user)));
+            Mail::to($user->email)->send(new ChangePasswordMail($user));
+            return redirect('/changePassword')->with('success', 'Password Changed Successfully! and sent to your email address');
+            
+        }
+        else
+        {
+            return Redirect::back()->with('danger', 'Failed to reset password');
+        }
    
-        return redirect('/home')->with('success', 'Password Changed Successfully!');
+        // return redirect('/home')->with('success', 'Password Changed Successfully!');
     }
 }
